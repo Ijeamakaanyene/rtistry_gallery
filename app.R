09a85bcd856876f98a435af77158976db007af78
Ijeamaka Anyene <Ijeamaka.Anyene@gmail.com>
2021-03-21 23:35:46 -0700

    added pagination, changed logo to gif, pagination fixed tweet loading issue

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
 library(shiny)
+library(shinycssloaders)
 library(dplyr)
+library(reactable)
+library(shinyPagerUI)
 
 source(here::here("R", "functions.R"))
 
@@ -7,7 +10,7 @@ tweet_data = readr::read_rds(here::here("data", "tweet_data.rds"))
 username_info = readr::read_rds(here::here("data", "username_art.rds"))
 
 ### Pulling in logo - shoutout to Sharla for the code
-logo = a(href = "", img(src = "logo.png", alt = "rtistry art gallery", height = "75px"))
+logo = a(href = "", img(src = "52ljrv.gif", alt = "rtistry art gallery", height = "75px"))
 
 ui = tagList(
   
@@ -20,67 +23,109 @@ ui = tagList(
     title = logo,
     windowTitle = "rtistry art gallery",
   
-  ### About Us Panel
+  ### About Panel
   tabPanel(
-    "about us",
-    h1("ABOUT US"),
+    "about the gallery",
+    h2("ABOUT THE GALLERY"),
     hr(),
-    p("rtistry art gallery is a shiny app celebrating and showcasing the aRt community. This gallery features tweets posted using the #rtistry hashtag from January 1st, 2021 to March 20th, 2021."),
+    p("rtistry art gallery is a shiny app celebrating and showcasing art in all its forms created by the #rstats community. This gallery features tweets posted using the #rtistry hashtag from January 1st, 2021 to March 20th, 2021."),
     br(),
     br(),
-    h1("FEATURED ARTISTS"),
+    h2("FEATURED ARTISTS"),
     hr(),
-    # UI output that creates artist cards
-    uiOutput("artist_card")
-           ),
+    # UI output featuring artists cards
+    uiOutput("artist_card")),
   
-  # Twitter Collections
+  ### Twitter Collections
   tabPanel(
     "collections",
+    id = "collections",
     column(
-      width = 8,
-      h1("FEATURED COLLECTIONS"),
-      tags$head(
-        tags$script(async = NA, src = "https://platform.twitter.com/widgets.js")
+      width = 12,
+      h2("FEATURED COLLECTIONS"),
+      hr(),
+      tags$head(tags$script(async = NA, src = "https://platform.twitter.com/widgets.js"))
+      ),
+    column(
+      width = 5,
+      tags$div(
+        class = "well artist-card",
+        h3("Selected Works"),
+        p("Filter to specific aRtists of interests, or leave as all."),
+        selectInput("filter", 
+                    label = NULL,
+                  choices = c("All", pull_artists(tweet_data)),
+                  selected = "All"),
+        p("Click through to view all #rtistry pieces."),
+        pageruiInput('pager', 1, nrow(tweet_data))
       )
     ),
     column(
-      width = 4,
-      selectInput("filter", 
-                  label = h1("Filter by aRtist |"), 
-                  choices = c("All", pull_artists(tweet_data)),
-                  selected = "All"
-                  )
-    ),
-    column(
-      width = 12,
-      hr()
-    ),
-    column(
-      width = 12,
-      wellPanel(
-        uiOutput("tweet")
+      width = 7,
+      tags$div(
+        class = "well picture-frame",
+          uiOutput("tweet")
+        )
+        )
+        
       )
     )
-  )
-  )
+
 )
+
 
 
 server = function(input, output, session) {
   
-  output$tweet = renderUI({
+  #### Collections
+  # Filter the data
+  filtered_tweet_data = reactive({
     if(input$filter == "All"){
       
-      tweet_data %>%
-        purrr::pmap(create_tweet_card)
+      tweet_data 
       
     } else {
       
       tweet_data %>%
-        filter(username == input$filter) %>%
-        purrr::pmap(create_tweet_card)
+        filter(username == input$filter)
     }
+  })
+  
+  # Reduce dataset to page selected
+  output_tweet_data = reactive({
+    
+    filtered_tweet_data() %>%
+        slice(input$pager$page_current) 
+    
+  })
+  
+  # Triggers when clicks tabpanel collections
+  observeEvent(
+    eventExpr = {
+      c(input$collections, input$filter)
+    },
+    
+    handlerExpr = {
+      
+      pages_total = nrow(filtered_tweet_data())
+      page_current = input$pager$page_current
+      if (input$pager$page_current > pages_total) {
+        page_current = pages_total
+      }
+      
+      updatePageruiInput(
+        session, 'pager',
+        page_current = page_current,
+        pages_total = pages_total
+      )
+    }
+  )
+
+  
+  output$tweet = renderUI({
+    
+      output_tweet_data() %>%
+        purrr::pmap(create_tweet_card)
     
   })
   
